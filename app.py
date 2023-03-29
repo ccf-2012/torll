6605ede6f3fc134ce5f7899cb09110f1953b54a4
ccf2012 <crlf66@163.com>
2023-03-29 21:22:54 +0800

    logview, sitenewlink

--- app.py
+++ app.py
@@ -2103,6 +2103,7 @@ class PtSite(db.Model):
             'last_update': self.last_update,
             'site': self.site,
             'cookie': self.cookie,
+            'siteNewLink': self.siteNewLink,
             'lastResultCount': self.lastResultCount,
             'newTorCount': siteCount(self.site),
             'lastNewStatus': siteCountToday(self.site),
@@ -2228,6 +2229,46 @@ def apiSitesData():
         'recordsTotal': PtSite.query.count(),
         'draw': request.args.get('draw', type=int),
     }
+
+# https://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-similar-to-tail
+def tail(f, lines=1, _buffer=4098):
+    """Tail a file and get X lines from the end"""
+    # place holder for the lines found
+    lines_found = []
+
+    # block counter will be multiplied by buffer
+    # to get the block size from the end
+    block_counter = -1
+
+    # loop until we find X lines
+    while len(lines_found) < lines:
+        try:
+            f.seek(block_counter * _buffer, os.SEEK_END)
+        except IOError:  # either file is too small, or too many lines requested
+            f.seek(0)
+            lines_found = f.readlines()
+            break
+
+        lines_found = f.readlines()
+
+        # we found enough lines, get out
+        # Removed this line because it was redundant the while will catch
+        # it, I left it for history
+        # if len(lines_found) > lines:
+        #    break
+
+        # decrement the block counter to get the
+        # next X bytes
+        block_counter -= 1
+
+    return lines_found[-lines:]
+
+@app.route('/logview')
+def logview():
+    with open("torll.log", "r") as f: 
+        lines = tail(f, 100)
+    # return render_template('logview.html')
+    return render_template("logview.html", content=''.join(lines))
 
 
 def siteNewsJob():
